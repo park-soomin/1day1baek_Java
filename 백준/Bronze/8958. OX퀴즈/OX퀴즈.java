c83c698bdaccdc981863384c4b45f75d9d9d3888
Park Soomin <soomini030113@gmail.com>
2026-03-27 20:35:25 +0900

    [Bronze II] Title: OX퀴즈, Time: 120 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int test = Integer.parseInt(bufferedReader.readLine());
+        int success = 0;
+        int sum = 0;
+
+        for (int i=0; i<test; i++) {
+            String str = bufferedReader.readLine();
+
+            for (int j=0; j<str.length(); j++) {
+                if (str.charAt(j) == 'O') {
+                    success += 1;
+                    sum += success;
+                } else {
+                    success = 0;
+                }
+            }
+
+            System.out.println(sum);
+            success = 0;
+            sum = 0;
+        }
+    }
+}
