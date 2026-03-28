d305916855c6daf3e57dd8d3371d191c56f9246a
Park Soomin <soomini030113@gmail.com>
2026-03-28 14:26:57 +0900

    [Bronze III] Title: 직각삼각형, Time: 96 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -6,10 +6,11 @@ import java.util.StringTokenizer;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
 
         int a, b, c;
-        boolean bool = true;
-        while(bool) {
+
+        while(true) {
             StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             a = Integer.parseInt(stringTokenizer.nextToken());
             b = Integer.parseInt(stringTokenizer.nextToken());
@@ -20,11 +21,12 @@ class Main {
             }
 
             if (a*a==b*b+c*c || b*b==a*a+c*c || c*c==a*a+b*b) {
-                System.out.println("right");
+                stringBuilder.append("right").append("\n");
             } else {
-                System.out.println("wrong");
+                stringBuilder.append("wrong").append("\n");
             }
         }
 
+        System.out.println(stringBuilder);
     }
 }
