d999a98c2deaa132ca68c0e4658dda31c2bf1b8e
Park Soomin <soomini030113@gmail.com>
2026-03-31 11:47:52 +0900

    [Bronze II] Title: 분해합, Time: 132 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -6,23 +6,22 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int num = Integer.parseInt(bufferedReader.readLine());
+        int num = Integer.parseInt(bufferedReader.readLine()); // 입력받는 숫자
 
         for (int i=1; i<num; i++) {
-            int temp = i;
-            int sum = temp;
+            int temp = i; // 1부터 차례대로 검사
+            int sum = temp; // 우선 sum에 예상되는 생성자 집어넣음
 
             while (temp > 0) {
-                sum += temp % 10;
-                temp /= 10;
+                sum += temp % 10; // 예상되는 생성자의 각 자릿수 더하기
+                temp /= 10; // 마지막 자릿수 제거 용
             }
 
             if (num == sum) {
                 System.out.println(i);
-                return;
+                return; // 가장 작은 생성자 찾았으니 바로 종료
             }
         }
-
-        System.out.println("0");
+        System.out.println("0"); // 생성자 없을 경우 0 출력
     }
 }
