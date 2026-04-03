c812893fe0454a0590233fe31dc26f7a7d27ba58
Park Soomin <soomini030113@gmail.com>
2026-04-03 14:07:52 +0900

    [Bronze V] Title: Adding Trouble, Time: 108 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int A = Integer.parseInt(stringTokenizer.nextToken());
+        int B = Integer.parseInt(stringTokenizer.nextToken());
+        int C = Integer.parseInt(stringTokenizer.nextToken());
+
+        if (A + B == C) {
+            System.out.println("correct!");
+        } else {
+            System.out.println("wrong!");
+        }
+    }
+}
