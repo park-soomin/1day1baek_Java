965c1cb920f6409591f4661cf79359d7f68a0d24
Park Soomin <soomini030113@gmail.com>
2026-04-06 11:08:07 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int A = Integer.parseInt(stringTokenizer.nextToken());
+        int B = Integer.parseInt(stringTokenizer.nextToken());
+        int V = Integer.parseInt(stringTokenizer.nextToken());
+
+        int days = (V-B) / (A-B);
+        if ((V-B) % (A-B) != 0) {
+            days++;
+        }
+
+        System.out.println(days);
+    }
+}
