48c051d56aed3557b54f0dcdce96c8ecc0a1bc57
Park Soomin <soomini030113@gmail.com>
2026-09-10 11:59:16 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.08 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string.substring(0,s));
+        
+        for (int i=e; i>=s; i--) {
+            sb.append(my_string.charAt(i));
+        }
+        
+        sb.append(my_string.substring(e+1));
+        
+        return sb.toString();
+    }
+}
